89e4c5d3732b5975932629c47fc53b448672e948
Matt Apperson <matt@appersonlabs.com>
2015-05-27 08:36:44 -0400

    Added tests

--- test/init.js
+++ test/init.js
@@ -3,9 +3,21 @@ module.exports = require('should');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = require('rc')('loopback', {test: {aerospike: {}}}).test.aerospike;
+var suite = require('loopback-datasource-juggler/test/persistence-hooks.suite.js');
 
 if (process.env.CI) {
   config = {
     host: 'localhost'
   };
 }
+
+global.getDataSource = global.getSchema = function(customConfig) {
+  var db = new DataSource(require('../'), customConfig || config);
+  db.log = function(a) {
+    console.log(a);
+  };
+
+  return db;
+};
+
+suite(global.getDataSource(), module.exports);
